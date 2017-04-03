c0278181fc4ee4c9b56af8c9926299548431b0cb
rhwayfun <rhwayfun@gmail.com>
2017-04-03 10:57:44 +0800

    修改依赖

--- src/test/java/com/rhwayfun/mockito/MockitoMockTest.java
+++ src/test/java/com/rhwayfun/mockito/MockitoMockTest.java
@@ -1,4 +1,4 @@
-package com.rhwayfun;
+package com.rhwayfun.mockito;
 
 import org.junit.Before;
 import org.junit.Test;
--- src/test/java/com/rhwayfun/mockito/MockitoTest.java
+++ src/test/java/com/rhwayfun/mockito/MockitoTest.java
@@ -1,4 +1,4 @@
-package com.rhwayfun;
+package com.rhwayfun.mockito;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
