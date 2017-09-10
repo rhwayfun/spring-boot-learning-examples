dcff3c4ef68c771fcd6c24a54d49a58bda1cbf38
rhwayfun <rhwayfun@gmail.com>
2017-09-10 20:36:12 +0800

    update test

--- spring-boot-quickstart/src/test/java/com/rhwayfun/springboot/quickstart/web/DemoControllerTest.java
+++ spring-boot-quickstart/src/test/java/com/rhwayfun/springboot/quickstart/web/DemoControllerTest.java
@@ -8,10 +8,12 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletContext;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,12 +29,14 @@ public class DemoControllerTest {
 
     @Test
     public void now() throws Exception {
-        this.mvc.perform(new RequestBuilder() {
+        ResultActions resultActions = this.mvc.perform(new RequestBuilder() {
             @Override
             public MockHttpServletRequest buildRequest(ServletContext servletContext) {
                 return new MockHttpServletRequest(RequestMethod.GET.name(), "/now");
             }
         }).andExpect(status().isOk()).andExpect(content().string("hello"));
+        String contentAsString = resultActions.andReturn().getResponse().getContentAsString();
+        assertEquals(contentAsString, "hello");
     }
 
 }
