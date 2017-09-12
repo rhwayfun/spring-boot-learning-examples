ab64e4ab8fb3230d9d6dc9d98b8efdb7d167457c
happyxiaofan <rhwayfun@gmail.com>
2017-09-12 22:31:04 +0800

    常量

--- spring-boot-mybatis/src/main/java/com/rhwayfun/springboot/mybatis/constants/DataSourceConstants.java
+++ spring-boot-mybatis/src/main/java/com/rhwayfun/springboot/mybatis/constants/DataSourceConstants.java
@@ -1,6 +1,6 @@
 package com.rhwayfun.springboot.mybatis.constants;
 
-public abstract class DataSourceConstants {
+public class DataSourceConstants {
 
     public static final String MAPPER_PACKAGE = "com.rhwayfun.springboot.mybatis.mapper";
     public static final String MAPPER_LOCATION = "classpath:mybatis/*Mapper*.xml";
