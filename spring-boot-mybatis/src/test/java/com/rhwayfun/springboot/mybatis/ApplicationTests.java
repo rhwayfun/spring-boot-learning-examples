025460172105ec0e91f026331f075c6925cf75de
happyxiaofan <rhwayfun@gmail.com>
2017-09-12 22:20:53 +0800

    更新数据库配置

--- spring-boot-mybatis/src/test/java/com/rhwayfun/springboot/mybatis/ApplicationTests.java
+++ spring-boot-mybatis/src/test/java/com/rhwayfun/springboot/mybatis/ApplicationTests.java
@@ -2,12 +2,13 @@ package com.rhwayfun.springboot.mybatis;
 
 import com.rhwayfun.springboot.mybatis.entity.User;
 import com.rhwayfun.springboot.mybatis.mapper.UserMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
@@ -22,6 +23,23 @@ public class ApplicationTests {
 	@Autowired
 	private UserMapper userMapper;
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Before
+	public void setup() {
+		String sql = "CREATE TABLE IF NOT EXISTS `user` (\n" +
+				"  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增，主键',\n" +
+				"  `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',\n" +
+				"  `user_name` varchar(15) DEFAULT NULL,\n" +
+				"  `age` int(11) DEFAULT NULL COMMENT '年龄',\n" +
+				"  `birth` date DEFAULT NULL COMMENT '生日',\n" +
+				"  PRIMARY KEY (`id`),\n" +
+				"  UNIQUE KEY `id_UNIQUE` (`id`)\n" +
+				") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用户信息表';\n";
+		jdbcTemplate.update(sql);
+	}
+
 	@Test
 	public void insertTest() {
 		User user = new User();
@@ -29,7 +47,7 @@ public class ApplicationTests {
 		user.setUserName("springboot");
 		user.setAge(1);
 		user.setBirth(new Date());
-		assertTrue(userMapper.insert(user) > 0);
+		assertTrue(userMapper.insertSelective(user) > 0);
 	}
 
 }
