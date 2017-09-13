ce747719819fbf988154c12c891569e4b9dfd177
happyxiaofan <rhwayfun@gmail.com>
2017-09-13 19:16:23 +0800

    更新测试用例

--- spring-boot-mybatis/src/main/java/com/rhwayfun/springboot/mybatis/constants/DataSourceConstants.java
+++ spring-boot-mybatis/src/main/java/com/rhwayfun/springboot/mybatis/constants/DataSourceConstants.java
@@ -1,6 +1,6 @@
 package com.rhwayfun.springboot.mybatis.constants;
 
-public class DataSourceConstants {
+public abstract class DataSourceConstants {
 
     public static final String MAPPER_PACKAGE = "com.rhwayfun.springboot.mybatis.mapper";
     public static final String MAPPER_LOCATION = "classpath:mybatis/*Mapper*.xml";
--- spring-boot-mybatis/src/test/java/com/rhwayfun/springboot/mybatis/ApplicationTests.java
+++ spring-boot-mybatis/src/test/java/com/rhwayfun/springboot/mybatis/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.rhwayfun.springboot.mybatis;
 
 import com.rhwayfun.springboot.mybatis.entity.User;
+import com.rhwayfun.springboot.mybatis.entity.UserExample;
 import com.rhwayfun.springboot.mybatis.mapper.UserMapper;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -48,6 +51,77 @@ public class ApplicationTests {
 		user.setAge(1);
 		user.setBirth(new Date());
 		assertTrue(userMapper.insertSelective(user) > 0);
+	}
+
+	@Test
+	public void insertTest2() {
+		User user = new User();
+		user.setUserId(2L);
+		user.setUserName("springboot2");
+		user.setAge(1);
+		user.setBirth(new Date());
+		assertTrue(userMapper.insert(user) > 0);
+	}
+
+	@Test
+	public void queryTest() {
+		List<User> users = userMapper.selectByExample(new UserExample());
+		if (!CollectionUtils.isEmpty(users)) {
+			assertTrue(userMapper.selectByPrimaryKey(users.get(0).getId()).getAge() == 1);
+		}
+	}
+
+	@Test
+	public void countTest() {
+		assertTrue(userMapper.countByExample(new UserExample()) > 0);
+	}
+
+	@Test
+	public void updateTest() {
+		List<User> users = userMapper.selectByExample(new UserExample());
+		if (!CollectionUtils.isEmpty(users)) {
+			User user = users.get(users.size() - 1);
+			user.setUserName("updateTest");
+			assertTrue(userMapper.updateByPrimaryKeySelective(user) > 0);
+		}
+	}
+
+	@Test
+	public void updateTest2() {
+		List<User> users = userMapper.selectByExample(new UserExample());
+		if (!CollectionUtils.isEmpty(users)) {
+			User user = users.get(users.size() - 1);
+			user.setUserName("updateTest2");
+			assertTrue(userMapper.updateByPrimaryKey(user) > 0);
+		}
+	}
+
+	@Test
+	public void deleteTest() {
+		List<User> users = userMapper.selectByExample(new UserExample());
+		if (!CollectionUtils.isEmpty(users)) {
+			User user = users.get(0);
+			assertTrue(userMapper.deleteByPrimaryKey(user.getId()) > 0);
+		}
+	}
+
+	@Test
+	public void deleteTest2() {
+		UserExample example = new UserExample();
+		example.createCriteria().andUserNameEqualTo("springboot");
+		assertTrue(userMapper.deleteByExample(example) > 0);
+	}
+
+	@Test
+	public void distinctOrTest() {
+		UserExample example = new UserExample();
+		example.setDistinct(true);
+		example.setOrderByClause("order by id asc");
+		UserExample.Criteria criteria = example.createCriteria();
+		criteria.andAgeIsNotNull();
+		UserExample.Criteria or = example.or();
+		or.andIdIsNotNull();
+		userMapper.selectByExample(example);
 	}
 
 }
